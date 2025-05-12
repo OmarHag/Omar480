ea4770645b5410d8d5444538893e9d298e57a1b3
omarhag <omarhag396@gmail.com>
2025-05-11 17:53:58 -0700

    11

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 function sortList() {
-    const list = document.getElementById("interest-list");
-    const items = Array.from(list.getElementsByTagName("li"));
-    const sortedItems = items.sort((a, b) =>
-      a.textContent.localeCompare(b.textContent)
-    );
-  
-    list.innerHTML = "";
-    sortedItems.forEach((item) => list.appendChild(item));
-  }
-  
+  const list = document.getElementById("interest-list");
+  const items = Array.from(list.getElementsByTagName("li"));
+  const sortedItems = items.sort((a, b) => a.textContent.localeCompare(b.textContent));
+  list.innerHTML = "";
+  sortedItems.forEach((item) => list.appendChild(item));
+}
